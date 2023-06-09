27cecd8464ec7275b618adc86beff4792b5ad14d
jorgenbuilder <jorgen@buildnode.io>
2023-06-09 10:14:39 -0700

    rotate table for portrait view

--- components/table.tsx
+++ components/table.tsx
@@ -1,10 +1,12 @@
 import { useTexture } from "@react-three/drei";
+import { useArbitraryStore } from "../state/zustand";
 
 export default function Table() {
   const texture = useTexture("/table.png");
   const ratio = 10 / 17.5;
+  const { portrait } = useArbitraryStore();
   return (
-    <mesh>
+    <mesh rotation={[0, 0, portrait ? Math.PI / 2 : 0]}>
       <meshStandardMaterial map={texture} />
       <planeGeometry args={[12, 12 * ratio]} />
     </mesh>
